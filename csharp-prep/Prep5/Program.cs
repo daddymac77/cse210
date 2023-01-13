d6ff6e19c68f59ca61ed514e6613fb2235afce0b
daddymac77 <cri22004@byui.edu>
2023-01-14 05:45:46 +0800

    prep assignment submitted

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -3,7 +3,36 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep5 World!");
+    {    
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNUmber();
+        DisplayResult(userName, userNumber);
     }
-}
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Hello Prep5 World!");
+            }
+        static string PromptUserName()
+        {
+            Console.WriteLine("What is your name: ");
+            string name = Console.ReadLine();
+            return name;
+            }
+        static int PromptUserNUmber()
+        {
+            Console.WriteLine("What is you favorite number:");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+            }
+        static int SquareNumber(int number)
+        {
+            int square = number * number;
+            return square;
+            } 
+        static void  DisplayResult(string name, int square)
+        {
+            Console.WriteLine($"{name}'s favorite number if squared is {square}");
+            }   
+    }
